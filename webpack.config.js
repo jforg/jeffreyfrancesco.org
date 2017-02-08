dc41755b84553420fb76492c270438b0dc2c7a79
Jeffrey Francesco <jeffrey.francesco@gmail.com>
2017-02-09 01:35:05 +0900

    Add webpack.config.js (development base)

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+// path
+const path = require('path');
+const publicPath = '/assets/';
+
+// loaders
+const urlLoader = {
+  loader: 'url-loader',
+  options: {
+    name: '[name].ext',
+    limit: 1024
+  }
+};
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    sourceMap: true
+  }
+};
+const sassLoader = {
+  loader: 'sass-loader',
+  options: {
+    sourceMap: true
+  }
+};
+const styleLoader = {
+  loader: 'style-loader'
+};
+
+// config
+module.exports = {
+  entry: {
+    style: 'stylesheets'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/assets'),
+    filename: '[name].js',
+    publicPath: publicPath
+  },
+  resolve: {
+    modules: [ path.resolve(__dirname, 'assets'), 'node_modules' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg)$/,
+        use: [ urlLoader ]
+      },
+      {
+        test: /\.css$/,
+        use: [ styleLoader, cssLoader ]
+      },
+      {
+        test: /\.scss$/,
+        use: [ styleLoader, cssLoader, sassLoader ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'gh-pages'),
+    publicPath: publicPath,
+    watchContentBase: true
+  }
+};
