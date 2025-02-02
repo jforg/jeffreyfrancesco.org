8ed94935d51af6fac91c3c04f97424f37348efcd
Jeffrey Francesco <jeffrey.francesco@gmail.com>
2025-02-02 14:40:18 +0900

    Add affiliate link banners before footer

--- assets/stylesheets.js
+++ assets/stylesheets.js
@@ -1,4 +1,5 @@
 import "normalize.css";
 import "./stylesheets/style.scss";
 import "./stylesheets/module/page_toc.css";
+import "./stylesheets/module/footer_ads.css";
 import "./stylesheets/highlight/tomorrow-night-eighties.css";
